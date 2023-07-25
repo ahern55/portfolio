77d7cbb30174456415d59120955c4e49add08912
Jason Ahern <jason.ahern53@gmail.com>
2023-07-25 12:02:39 -0400

    changing navbar animation for responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,29 +7,6 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 const Home: NextPage = () => {
   gsap.registerPlugin(ScrollTrigger);
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const element = ref.current;
-    if (element) {
-      gsap.fromTo(
-        element.querySelector("#nav-bar"),
-        {
-          opacity: 0,
-          y: 0,
-        },
-        {
-          opacity: 1,
-          y: 0,
-          scrollTrigger: {
-            start: "top top",
-            end: `+${window.innerHeight}`,
-            scrub: true,
-          },
-        }
-      );
-    }
-  }, []);
 
   return (
     <>
@@ -38,7 +15,7 @@ const Home: NextPage = () => {
         <meta property="og:title" content="Jason" key="title" />
       </Head>
       <main>
-        <div ref={ref}>
+        <div>
           <NavBar />
           <div className="h-screen flex" id="name-screen">
             <div className="m-auto text-5xl sm:text-6xl">Jason Ahern</div>
---
+++ utils/screenSizes.ts
@@ -0,0 +1,8 @@
+// kinda hacky, but the best way to do this now so that I don't have random pixel values floating around...
+// I could not get typescript to let me access tailwind config values as in https://tailwindcss.com/docs/configuration#referencing-in-java-script
+// so here are the fun values hardcoded from https://tailwindcss.com/docs/responsive-design
+
+export const smallScreenWidthPixels = 640;
+export const mediumScreenWidthPixels = 768;
+export const largeScreenWidthPixels = 1024;
+export const extraLargeScreenWidthPixels = 1280;
