82402530198da14237357368f1a5b2acc7c79638
Jason Ahern <jason.ahern53@gmail.com>
2023-07-31 14:28:02 -0400

    responsiveness

--- components/timeline/TimelineCard.tsx
+++ components/timeline/TimelineCard.tsx
@@ -5,13 +5,13 @@ const TimelineCard = (props: { resumeEntry: resumeEntry }) => {
     <>
       <div className="w-full">
         <div className="bg-transparent rounded p-4 flex flex-col leading-normal">
-          <p className="text-base secondary ">{props.resumeEntry.dateString}</p>
+          <p className="text-base secondary">{props.resumeEntry.dateString}</p>
           <div className="border-t secondary-border w-[90%] lg:w-2/3"></div>
           <div className="">
-            <div className="font-bold text-2xl mb-1">
+            <div className="font-bold text-xl md:text-2xl mb-1">
               {props.resumeEntry.title}
             </div>
-            <div className="font-bold text-xl mb-2">
+            <div className="font-bold text-lg md:text-xl mb-2">
               {props.resumeEntry.subtitle}
             </div>
           </div>
